4331102e9ea50a06e5b81ee2e8dedc12affa0074
mathieu <mathieu.sylvain@phenos.ca>
2015-10-24 10:33:30 -0400

    Added serve task to bulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
+var serve = require('gulp-serve');
 
-gulp.task('default', function() {
-    gulp.src('./src/app/less/**/*.less')
-        .pipe(less({
-            //paths: [ path.join(__dirname, 'less', 'includes') ]
-        }))
+var paths = {
+    webroot: '.',
+    less: './src/app/less/**/*.less'
+};
+
+gulp.task('serve', serve({
+    root: [paths.webroot],
+    port: 8001
+}));
+
+gulp.task('less', function() {
+    gulp.src(paths.less)
+        .pipe(less())
         .pipe(gulp.dest('./src/app/css'));
 });
 
-gulp.watch('./src/app/less/*.less', ['default']);
+gulp.task('watch', function() {
+    gulp.watch('./src/app/less/*.less', ['less']);
+});

5b7a9ead2de8c4c54f39beab05780853e7fd9638
mathieu <mathieu.sylvain@phenos.ca>
2015-11-15 11:05:34 -0500

    Removed build files from sources

--- build/static/js/module.js
+++
@@ -1,9 +0,0 @@
-angular.module('mindgame', [
-    'angular-uri',
-    'ngAnimate',
-    'yarn',
-    'ui.router',
-    'luegg.directives',
-    'cfp.hotkeys',
-    'breakpointApp'
-]);
--- build/static/js/root.js
+++
@@ -1,7 +0,0 @@
-angular.module('mindgame').controller('root', rootController);
-
-function rootController(metadata, loadGameScripts, $scope) {
-    $scope.metadata = metadata;
-    console.info("Yarn game started!");
-    loadGameScripts();
-}

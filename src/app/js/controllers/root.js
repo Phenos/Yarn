a437e4c76b786e287010da5fa77f70d74dd169ce
mathieu <mathieu.sylvain@phenos.ca>
2015-11-15 19:55:51 -0500

    Added sound library and sample sounds on button clicks and an ambient creep sound!

--- src/app/js/controllers/root.js
+++ src/app/js/controllers/root.js
@@ -1,7 +1,13 @@
 angular.module('mindgame').controller('root', rootController);
 
-function rootController(metadata, loadGameScripts, $scope) {
+function rootController(metadata, loadGameScripts, $scope, ngAudio) {
     $scope.metadata = metadata;
     console.info("Yarn game started!");
     loadGameScripts();
+
+    var ambientSound = ngAudio.load("./sounds/166187__drminky__creepy-dungeon-ambience.mp3");
+    // todo: Load sound ambience from story
+    ambientSound.play();
+    ambientSound.loop = true;
+
 }
--- src/app/js/module.js
+++ src/app/js/module.js
@@ -1,4 +1,5 @@
 angular.module('mindgame', [
+    'ngAudio',
     'angular-uri',
     'ngAnimate',
     'yarn',

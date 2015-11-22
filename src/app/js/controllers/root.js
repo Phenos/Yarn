de7cb633e2b9520153caecd84ed2fc86c2ade080
mathieu <mathieu.sylvain@phenos.ca>
2015-11-21 23:21:39 -0500

    Non script command are now prefixed with a colon ":"

--- src/app/js/controllers/root.js
+++ src/app/js/controllers/root.js
@@ -1,10 +1,11 @@
 angular.module('mindgame').controller('root', rootController);
 
 function rootController(metadata, loadGameScripts, $scope, ngAudio, yConsole, loadMetadata) {
+
     $scope.metadata = metadata;
     loadMetadata().then(function (metadata) {
         yConsole.log("Welcome to <strong>Yarn</strong> <em>v" + metadata.version + "</em>");
-        yConsole.log('Type <strong>CTRL+H</strong> or enter "<strong>help</strong>" in the command-line bellow to see available commands!')
+        yConsole.log('Type <strong>CTRL+H</strong> or enter "<strong>:help</strong>" in the command-line bellow to see available commands!')
     });
     loadGameScripts();
 

a4f09b1f28d2b7ad410cb2c18db092ab3798d9b0
mathieu <mathieu.sylvain@phenos.ca>
2016-01-01 13:44:07 -0500

    The look, move, take and inventory buttons are only displayed if they are needed / usable.

--- src/app/js/yarn/yarn.state.predicate.js
+++ src/app/js/yarn/yarn.state.predicate.js
@@ -23,10 +23,43 @@
              */
             this.syntax = function (text) {
                 this.label = text;
+                // todo: refctor this method is calling the parentdirectly instead of using a service or emmiting
+                // This prevents the method from being put on the prototype
                 yarn.syntax(this, text);
                 return this;
             }
+
         }
+
+
+        /**
+         * Filter an array of things depending if they have a matching predicate and object
+         * @param things
+         * @param object
+         * @returns {Array}
+         * Usage:
+         *  InventoryItemsInsideChest = predicateIsA.filter(ItemsInsideChest, 'InventoryItem');
+         */
+        // todo: refactor this method to work from a set/list of things
+        Predicate.prototype.filterThings = function (things, object) {
+            var self = this;
+            var filtered = [];
+            if (angular.isArray(things)) {
+                things.forEach(function (thing) {
+                    var assertions = thing.getAssertion(self);
+                    if (object) {
+                        assertions.forEach(function (assertion) {
+                            if (assertion.object === object) {
+                                filtered.push(thing);
+                            }
+                        });
+                    } else {
+                        if (assertions.length) filtered.push(thing);
+                    }
+                });
+            }
+            return filtered;
+        };
 
         return Predicate;
     }

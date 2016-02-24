0a2d36676043f6baf8c01de30caaaaa4708bd15e
mathieu <mathieu.sylvain@phenos.ca>
2016-02-24 11:33:20 -0500

    [PATIAL] Removing mode changes in the Runtime

--- src/app/js/yarn/yarn.runtime.js
+++ src/app/js/yarn/yarn.runtime.js
@@ -5,28 +5,18 @@
     function RuntimeService(Cursor,
                             yConsole) {
 
-        var runtimeModes = {
-            "when": true,
-            "do": true,
-            "on": true
-        };
-
-
         /**
          * Runtime class user to execute the ast with the state
          * @param ast
          * @param state
-         * @param onModeChange
          * @constructor
          * @return {string}
          */
-        function Runtime(ast, state, onModeChange) {
+        function Runtime(ast, state) {
             this.ast = ast;
             this.state = state;
             this.cursor = new Cursor();
             this.stack = new Stack();
-            // Allows the logic class to keep track on special key nodes
-            this.onModeChange = onModeChange || function () {};
         }
 
         // todo: See if code should be generalized between Stack/Pointer/Cursor
@@ -52,10 +42,10 @@
             return this.scopes[0];
         };
 
-        Stack.prototype.push = function (mode, obj) {
+        Stack.prototype.push = function (obj) {
             //console.log("Pushed : ", obj);
             var head = this.head();
-            this.scopes.push(new Scope(mode, obj, head));
+            this.scopes.push(new Scope(obj, head));
         };
 
         Stack.prototype.pop = function () {
@@ -63,10 +53,9 @@
             this.scopes.pop();
         };
 
-        function Scope(mode, obj, parent) {
+        function Scope(obj, parent) {
             this.values = obj;
             this.parent = parent;
-            this.values["$mode"] = mode;
         }
 
         /**
@@ -78,14 +67,11 @@
         };
 
         Runtime.prototype.runNode = function (node) {
-            var self = this;
-
             var returnValue;
-
-            var defaultMode = {
+            var nodeHandlers = {
                 "root": function (runtime, node) {
                     // Nothing to do really with the root instruction!
-                    runtime.stack.push("default", {
+                    runtime.stack.push({
                         "this": "root"
                     });
                     runtime.runSet(node.set);
@@ -108,7 +94,7 @@
                         yConsole.error("Compilation error: Unknown node variant [" + node.variant + "-" + node.type + "-" + node.value+ "]");
                     }
 
-                    runtime.stack.push("default", {
+                    runtime.stack.push({
                         "this": returnValue
                     });
                     runtime.runSet(node.set);
@@ -117,7 +103,7 @@
                 },
                 "value": function (runtime, node) {
                     returnValue = node.value;
-                    runtime.stack.push("default", {
+                    runtime.stack.push({
                         "this": node.value
                     });
                     runtime.runSet(node.set);
@@ -128,25 +114,10 @@
                     //console.log("instruction ", node.value);
                     var predicate;
                     var args;
-                    var mode;
                     var createdAssertions = [];
-                    // First, test if the instruction is for a mode change or a predicate
-                    var modeHandler = modes[node.value.trim().toLowerCase()];
-                    if (modeHandler) {
-                        mode = node.value.trim().toLowerCase();
-                        runtime.stack.push(mode, {
-                        });
+                    if (node.value === "@imported") {
+                        //console.log("Running imported node", node);
                         runtime.runSet(node.set);
-                        runtime.stack.pop();
-
-                    } else if (node.value === "@imported") {
-
-                        //runtime.stack.push("default", {
-                        //    "this": "root"
-                        //});
-                        runtime.runSet(node.set);
-                        //runtime.stack.pop();
-
                     } else {
                         var parent;
                         var parentObject;
@@ -200,160 +171,9 @@
                 }
             };
 
-            // TODO: Get rid of whenMode....
-            var whenMode = {
-                "root": function (runtime, node) {
-                    // Nothing to do really with the root instruction!
-
-                    //runtime.stack.push("default", {
-                    //    "this": "root"
-                    //});
-                    //runtime.runSet(node.set);
-                    //runtime.stack.pop();
-                },
-                "symbol": function (runtime, node) {
-                    // whenMode: Get or create a new thing according to that symbol
-                    if (node.variant === "value") {
-                        returnValue = node.value;
-                        //console.log("VALUE variant:", node.value);
-                    } else if (node.variant === "reference") {
-                        returnValue = runtime.state.thing(node.value);
-                    } else if (node.variant === "constant") {
-                        returnValue = runtime.state.thing(node.value);
-                    } else {
-                        console.error("Compilation error: Unknown node variant [" + node.variant + "]", node);
-                        yConsole.error("Compilation error: Unknown node variant [" + node.variant + "-" + node.type + "-" + node.value+ "]");
-                    }
-                    runtime.stack.push("when", {
-                        "this": returnValue
-                    });
-                    runtime.runSet(node.set);
-                    runtime.stack.pop();
-                    return returnValue;
-                },
-                "value": function (runtime, node) {
-                    runtime.stack.push("when", {
-                        "this": node.value
-                    });
-                    returnValue = node.value;
-                    runtime.runSet(node.set);
-                    runtime.stack.pop();
-                    return returnValue;
-                },
-                "instruction": function (runtime, node) {
-                    var predicate;
-                    var args;
-
-                    if (node.value === "do" ) {
-
-
-                        var parent = runtime.stack.parent();
-                        //debugger;
-                        args = runtime.runSet(node.set);
-                        parent.values["$do"] = args;
-
-                    } else {
-
-                        // Identify which predicate corresponds to this instruction
-                        predicate = runtime.state.predicate(node.value);
-                        node.resolvedTo = predicate;
-
-                        // Run the child set of node to be used by the predicate
-                        args = runtime.runSet(node.set);
-
-                        var head = runtime.stack.head();
-                        // Add a collection of action handler to add the "do" to then afterward
-                        var actionHandlers = head.values.actionHandlers = [];
-
-                        var doReferences = head.values["$do"] || [];
-
-                        // Create assertion from predicate
-                        //TODO: Instead of an "if", simply prefil the args with [undefined] if no args
-                        if (args.length) {
-                            args.forEach(function (arg) {
-                                //todo: Handle "non predicate" instructions such as "this/that", without creating new assertion
-                                var currentThis = runtime.stack.head().values.this;
-                                //console.log("runtime.stack.head().values", runtime.stack.head().values);
-                                doReferences.forEach(function (doReference) {
-                                    var actionHandler = runtime.state.setActionHandler(currentThis, predicate, arg, doReference);
-                                    actionHandlers.push(actionHandler);
-                                });
-                                //console.log("created assetion: ", arg);
-                            });
-                        } else {
-                            var currentThis = runtime.stack.head().values.this;
-                            doReferences.forEach(function (doReference) {
-                                var actionHandler = runtime.state.setActionHandler(currentThis, predicate, null, doReference);
-                                actionHandlers.push(actionHandler);
-                            });
-                        }
-
-                        //debugger;
-
-                    }
-                    return null;
-                },
-                "fallback": function (runtime, node) {
-                    console.error("Compilation error: Set ignored, unrecognised node type [" + node.type + "]", node);
-                    yConsole.error("Compilation error: Set ignored, unrecognised node type [" + node.type + "]");
-                    return null;
-                }
-            };
-
-            // Dont execute enything from a "on" node
-            var onMode = {
-                "root": function (runtime, node) {
-                },
-                "symbol": function (runtime, node) {
-                },
-                "value": function (runtime, node) {
-                },
-                "instruction": function (runtime, node) {
-                },
-                "fallback": function (runtime, node) {
-                }
-            };
-
-            var modes = {
-                "default": defaultMode,
-                "when": whenMode,
-                "do": {}, //TODO: Not yet implement... really needed ?
-                "on": onMode
-            };
-
-
             this.cursor.push(node);
-
-            var mode;
-            var head = this.stack.head();
-            if (!head) {
-                mode = "default";
-            } else {
-                mode = head.values["$mode"];
-                //console.log('-----head.values', mode, node.type, head.values);
-                if (!mode) mode = "default";
-
-                // Detect a mode change from parent node. Ex: when, on
-                var parent = head.parent;
-                if (parent) {
-                    var parentMode = head.parent.values["$mode"];
-                    if (!parentMode) parentMode = "default";
-                    if (mode !== parentMode) this.onModeChange(mode, node);
-                }
-
-            }
-
-
-            var nodeHandler = modes[mode][node.type];
-            //console.log("  nodeHandler :   ", nodeHandler);
-
-            if (!nodeHandler) {
-                nodeHandler = modes.default.fallback;
-            }
-
-
+            var nodeHandler = nodeHandlers[node.type];
             returnValue = nodeHandler(this, node);
-
 
             this.cursor.pop();
 

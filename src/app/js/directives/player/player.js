56c73c020dd4ba8f53691ff7e36b38aecf8bf55f
mathieu <mathieu.sylvain@phenos.ca>
2016-03-17 11:42:22 -0400

    FIXED: Player should not scroll when showing the Coverpage

--- src/app/js/directives/player/player.js
+++ src/app/js/directives/player/player.js
@@ -2,31 +2,57 @@ yarn.directive('player', function () {
 
     var EasingFunctions = {
         // no easing, no acceleration
-        linear: function (t) { return t },
+        linear: function (t) {
+            return t
+        },
         // accelerating from zero velocity
-        easeInQuad: function (t) { return t*t },
+        easeInQuad: function (t) {
+            return t * t
+        },
         // decelerating to zero velocity
-        easeOutQuad: function (t) { return t*(2-t) },
+        easeOutQuad: function (t) {
+            return t * (2 - t)
+        },
         // acceleration until halfway, then deceleration
-        easeInOutQuad: function (t) { return t<.5 ? 2*t*t : -1+(4-2*t)*t },
+        easeInOutQuad: function (t) {
+            return t < .5 ? 2 * t * t : -1 + (4 - 2 * t) * t
+        },
         // accelerating from zero velocity
-        easeInCubic: function (t) { return t*t*t },
+        easeInCubic: function (t) {
+            return t * t * t
+        },
         // decelerating to zero velocity
-        easeOutCubic: function (t) { return (--t)*t*t+1 },
+        easeOutCubic: function (t) {
+            return (--t) * t * t + 1
+        },
         // acceleration until halfway, then deceleration
-        easeInOutCubic: function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 },
+        easeInOutCubic: function (t) {
+            return t < .5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1
+        },
         // accelerating from zero velocity
-        easeInQuart: function (t) { return t*t*t*t },
+        easeInQuart: function (t) {
+            return t * t * t * t
+        },
         // decelerating to zero velocity
-        easeOutQuart: function (t) { return 1-(--t)*t*t*t },
+        easeOutQuart: function (t) {
+            return 1 - (--t) * t * t * t
+        },
         // acceleration until halfway, then deceleration
-        easeInOutQuart: function (t) { return t<.5 ? 8*t*t*t*t : 1-8*(--t)*t*t*t },
+        easeInOutQuart: function (t) {
+            return t < .5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t
+        },
         // accelerating from zero velocity
-        easeInQuint: function (t) { return t*t*t*t*t },
+        easeInQuint: function (t) {
+            return t * t * t * t * t
+        },
         // decelerating to zero velocity
-        easeOutQuint: function (t) { return 1+(--t)*t*t*t*t },
+        easeOutQuint: function (t) {
+            return 1 + (--t) * t * t * t * t
+        },
         // acceleration until halfway, then deceleration
-        easeInOutQuint: function (t) { return t<.5 ? 16*t*t*t*t*t : 1+16*(--t)*t*t*t*t }
+        easeInOutQuint: function (t) {
+            return t < .5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t
+        }
     }
 
     return {
@@ -40,9 +66,15 @@ yarn.directive('player', function () {
         controller: playerController
     };
 
-    function playerController($scope, $element, sidebar, writers, promptLoop, player) {
+    function playerController($scope,
+                              $element,
+                              sidebar,
+                              writers,
+                              promptLoop,
+                              player,
+                              state) {
 
-        var scrollAreaElem = $element[0].getElementsByClassName("player")[0]
+        var scrollAreaElem = $element[0].getElementsByClassName("player")[0];
 
         promptLoop.onUpdate(function (promptLoop) {
             // Load the appropriate prompt and setup the ui with the prompt
@@ -76,18 +108,23 @@ yarn.directive('player', function () {
         };
 
         this.scroll = function (offset) {
-            var _offset = 600;
-            if (!angular.isUndefined(offset)) {
-                _offset = offset;
-            }
-            //var scrollHeight = scrollAreaElem.scrollHeight + _offset;
-            var scrollTop = scrollAreaElem.scrollTop + _offset;
-            if (scrollTop > scrollAreaElem.scrollHeight) {
-                scrollTop = scrollAreaElem.scrollHeight;
-            }
+            // First we check to see if it's the first game step
+            // to prevent scrolling when first showing the coverpage
+            if (state.step() > 0) {
 
-            angular.element(scrollAreaElem)
-                .scrollTopAnimated(scrollTop, 1250, EasingFunctions.easeOutQuart())
+                var _offset = 600;
+                if (!angular.isUndefined(offset)) {
+                    _offset = offset;
+                }
+                //var scrollHeight = scrollAreaElem.scrollHeight + _offset;
+                var scrollTop = scrollAreaElem.scrollTop + _offset;
+                if (scrollTop > scrollAreaElem.scrollHeight) {
+                    scrollTop = scrollAreaElem.scrollHeight;
+                }
+
+                angular.element(scrollAreaElem)
+                    .scrollTopAnimated(scrollTop, 1250, EasingFunctions.easeOutQuart())
+            }
         };
 
     }

83217b440e7cb740d16e5903151461acebd659ab
mathieu <mathieu.sylvain@phenos.ca>
2016-03-20 15:41:29 -0400

    Graph tool panel is now live

--- src/app/js/directives/graph/graph.js
+++ src/app/js/directives/graph/graph.js
@@ -12,19 +12,30 @@ yarn.directive('graph', function GraphDirective(state, assert) {
     };
 
     function GraphController($element, $timeout, graph) {
+        var self = this;
         var thingIsA = this.thingIsA || "room";
         var predicate = this.predicate.toLowerCase() || "linksto";
         var force = d3.layout.force();
         var svg = d3.select($element[0]).append("svg");
+        var currentModel;
         //console.log("svg", svg);
 
         graph.register(this);
+        postal.subscribe({
+            channel: "runtime",
+            topic: "afterRun",
+            callback: function () {
+                self.update();
+            }
+        });
 
-        this.update = function (model) {
+        this.update = function (newModel) {
+            var model = newModel || currentModel;
+            currentModel = model;
             var self = this;
             svg.selectAll("*").remove();
             $timeout(function () {
-                var _model = model || self.models.rooms;
+                var _model = model || self.models.containers;
                 buildGraph(_model);
             }, 100);
         };
@@ -139,112 +150,49 @@ yarn.directive('graph', function GraphDirective(state, assert) {
         }
 
         function buildGraph(model) {
-            var modelData = model();
+            var modelData;
+            if (model) modelData = model();
+            if (modelData.nodes.length) {
 
-            var nodes = modelData.nodes;
-            var links = modelData.links;
-            var radius = 140;
+                var nodes = modelData.nodes;
+                var links = modelData.links;
+                var radius = 140;
 
-            var width = svg[0][0].clientWidth;
-            var height = svg[0][0].clientHeight;
+                var width = svg[0][0].clientWidth;
+                var height = svg[0][0].clientHeight;
 
-            //responsive SVG needs these 2 attributes and no width and height attr
-            svg.attr("preserveAspectRatio", "xMinYMin meet")
-                .attr("viewBox", "0 0 " + width + " " + height);
+                //responsive SVG needs these 2 attributes and no width and height attr
+                svg.attr("preserveAspectRatio", "xMinYMin meet")
+                    .attr("viewBox", "0 0 " + width + " " + height);
 
-            //console.log("width, height", width, height);
-            //console.log("thingIsA", thingIsA);
+                //console.log("width, height", width, height);
+                //console.log("thingIsA", thingIsA);
 
-            force
-                .nodes(nodes)
-                .links(links)
-                .size([width, height])
-                .gravity(0)
-                .linkDistance(radius)
-                //.chargeDistance(120)
-                .charge(-500)
-                .start();
+                force
+                    .nodes(nodes)
+                    .links(links)
+                    .size([width, height])
+                    .gravity(0)
+                    .linkDistance(radius)
+                    //.chargeDistance(120)
+                    .charge(-500)
+                    .start();
 
-            force.on("tick", function (e) {
+                force.on("tick", function (e) {
 
-                // soft-center the root node
-                var k = .01;
-                var nodes = force.nodes();
-                nodes[0].y += (h / 2 - nodes[0].y) * k;
-                nodes[0].x += (w / 2 - nodes[0].x) * k;
-            });
-
-            var link = svg
-                .selectAll(".link")
-                .data(links)
-                .enter()
-                .append("line")
-                .attr("class", "link")
-                .attr("x1", function (d) {
-                    return bound(d.source.x, 0, width);
-                })
-                .attr("y1", function (d) {
-                    return bound(d.source.y, 0, height);
-                })
-                .attr("x2", function (d) {
-                    return bound(d.target.x, 0, width);
-                })
-                .attr("y2", function (d) {
-                    return bound(d.target.y, 0, height);
-                })
-                .attr("transform", "scale(1)");
-
-            //var link = svg
-            //    .selectAll(".link")
-            //    .data(links)
-            //    .enter()
-            //    .append("line")
-            //    .attr("class", "link");
-            //
-            var linkLabel = svg
-                .append("svg:g")
-                .selectAll("g.linklabelholder")
-                .data(force.links());
-
-
-            linkLabel.enter()
-                .append("text")
-                .attr("class", "linklabel")
-                .text(function (d) {
-                    return d.predicate.id;
+                    // soft-center the root node
+                    var k = .01;
+                    var nodes = force.nodes();
+                    nodes[0].y += (h / 2 - nodes[0].y) * k;
+                    nodes[0].x += (w / 2 - nodes[0].x) * k;
                 });
 
-            var node = svg.selectAll(".node")
-                .data(nodes)
-                .enter()
-                .append("g")
-                .attr("class", "node")
-                .call(force.drag);
-
-            node
-                .append("circle")
-                .attr("r", 40);
-
-            node
-                .append("text")
-                .text(function (d) {
-                    return d.name.substring(0, 16);
-                });
-
-            function bound(val, min, max) {
-                return Math.max(min + (radius / 4), Math.min(max - (radius / 4), val))
-            }
-
-            force.on("tick", function () {
-                linkLabel
-                    .attr("y", function (d) {
-                        return (d.source.y + d.target.y) / 2;
-                    })
-                    .attr("x", function (d) {
-                        return (d.source.x + d.target.x) / 2;
-                    });
-
-                link
+                var link = svg
+                    .selectAll(".link")
+                    .data(links)
+                    .enter()
+                    .append("line")
+                    .attr("class", "link")
                     .attr("x1", function (d) {
                         return bound(d.source.x, 0, width);
                     })
@@ -259,17 +207,82 @@ yarn.directive('graph', function GraphDirective(state, assert) {
                     })
                     .attr("transform", "scale(1)");
 
-                node
-                    .attr("transform", function (d) {
-                        var x = bound(d.x, 0, width);
-                        var y = bound(d.y, 0, height);
-                        d.x = x;
-                        d.y = y;
-                        return "scale(1)translate(" + x + "," + y + ")";
+                //var link = svg
+                //    .selectAll(".link")
+                //    .data(links)
+                //    .enter()
+                //    .append("line")
+                //    .attr("class", "link");
+                //
+                var linkLabel = svg
+                    .append("svg:g")
+                    .selectAll("g.linklabelholder")
+                    .data(force.links());
+
+
+                linkLabel.enter()
+                    .append("text")
+                    .attr("class", "linklabel")
+                    .text(function (d) {
+                        return d.predicate.id;
                     });
 
-            });
+                var node = svg.selectAll(".node")
+                    .data(nodes)
+                    .enter()
+                    .append("g")
+                    .attr("class", "node")
+                    .call(force.drag);
 
+                node
+                    .append("circle")
+                    .attr("r", 40);
+
+                node
+                    .append("text")
+                    .text(function (d) {
+                        return d.name.substring(0, 16);
+                    });
+
+                function bound(val, min, max) {
+                    return Math.max(min + (radius / 4), Math.min(max - (radius / 4), val))
+                }
+
+                force.on("tick", function () {
+                    linkLabel
+                        .attr("y", function (d) {
+                            return (d.source.y + d.target.y) / 2;
+                        })
+                        .attr("x", function (d) {
+                            return (d.source.x + d.target.x) / 2;
+                        });
+
+                    link
+                        .attr("x1", function (d) {
+                            return bound(d.source.x, 0, width);
+                        })
+                        .attr("y1", function (d) {
+                            return bound(d.source.y, 0, height);
+                        })
+                        .attr("x2", function (d) {
+                            return bound(d.target.x, 0, width);
+                        })
+                        .attr("y2", function (d) {
+                            return bound(d.target.y, 0, height);
+                        })
+                        .attr("transform", "scale(1)");
+
+                    node
+                        .attr("transform", function (d) {
+                            var x = bound(d.x, 0, width);
+                            var y = bound(d.y, 0, height);
+                            d.x = x;
+                            d.y = y;
+                            return "scale(1)translate(" + x + "," + y + ")";
+                        });
+
+                });
+            }
         }
     }
 });

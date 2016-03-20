fa1255880f76f046b11eb9b925dbac3cb5ce4f67
mathieu <mathieu.sylvain@phenos.ca>
2016-03-20 15:23:16 -0400

    Things panel now has filters and is live

--- src/app/js/directives/tools/things.js
+++ src/app/js/directives/tools/things.js
@@ -1,74 +1,93 @@
-yarn.directive('thingsTool', function ThingsToolDirective(things,
-                                                          state,
-                                                          postal) {
+yarn.directive('thingsTool', ThingsToolDirective);
 
+function ThingsToolDirective(things,
+                             state,
+                             postal) {
     return {
         restrict: 'E',
         scope: {},
         replace: true,
         templateUrl: './html/tools/things.html',
-        controller: Controller
-    };
+        controller: function ($scope) {
 
-    function Controller($scope) {
+            $scope.thingsFiltered = [];
 
-        postal.subscribe({
-            channel: "runtime",
-            topic: "afterRun",
-            callback: function () {
-                $scope.update();
-            }
-        });
-
-        $scope.update = function () {
-            $scope.types = [];
-            $scope.typesIndex = {};
-            $scope.things = [];
-            $scope.thingsIndex = {};
-            $scope.assertions = [];
-
-            var allAssertions = state.assertions.all();
-            $scope.assertions = allAssertions.filter(function (assertion) {
-                var keep = false;
-                var type = $scope.typesIndex[assertion.object.id];
-                var thing = $scope.thingsIndex[assertion.subject.id];
-                if (assertion.predicate.id === "is") {
-                    if (!thing) {
-                        thing = {
-                            object: assertion.subject,
-                            count: 1
-                        };
-                        $scope.thingsIndex[assertion.subject.id] = thing;
-                        $scope.things.push(thing);
-                    } else {
-
-                    }
-                    if (!type) {
-                        type = {
-                            object: assertion.object,
-                            selected: true,
-                            count: 1
-                        };
-                        $scope.typesIndex[assertion.object.id] = type;
-                        $scope.types.push(type);
-                    } else {
-                        type.count++;
-                    }
-                    keep = true;
+            postal.subscribe({
+                channel: "runtime",
+                topic: "afterRun",
+                callback: function () {
+                    $scope.update();
                 }
-                return keep;
             });
 
-            $scope.types.sort(function (A, B) {
-                return (A.object.id > B.object.id) ? 1 : -1;
-            });
-            $scope.things.sort(function (A, B) {
-                return (A.object.id > B.object.id) ? 1 : -1;
-            });
-        };
+            $scope.filterByType = function (type) {
+                //console.log("filterByType", type);
+                $scope.filter = type.object.id;
+                $scope.thingsFiltered = $scope.applyFilter();
+            };
 
-        $scope.update();
-    }
+            $scope.applyFilter = function () {
+                var results;
+                if ($scope.filter) {
+                    results = $scope.things.filter(function (thing) {
+                        return thing.types.indexOf($scope.filter) > -1;
+                    });
+                } else {
+                    results = $scope.things;
+                }
+                return results;
+            };
 
-});
+            $scope.update = function () {
+                $scope.types = [];
+                $scope.typesIndex = {};
+                $scope.things = [];
+                $scope.thingsIndex = {};
+                $scope.assertions = [];
+
+                var allAssertions = state.assertions.all();
+                $scope.assertions = allAssertions.filter(function (assertion) {
+                    var keep = false;
+                    var type = $scope.typesIndex[assertion.object.id];
+                    var thing = $scope.thingsIndex[assertion.subject.id];
+                    if (assertion.predicate.id === "is") {
+                        if (!type) {
+                            type = {
+                                object: assertion.object,
+                                selected: true,
+                                count: 1
+                            };
+                            $scope.typesIndex[assertion.object.id] = type;
+                            $scope.types.push(type);
+                        } else {
+                            type.count++;
+                        }
+                        if (!thing) {
+                            thing = {
+                                object: assertion.subject,
+                                types: []
+                            };
+                            $scope.thingsIndex[assertion.subject.id] = thing;
+                            $scope.things.push(thing);
+                        }
+                        thing.types.push(type.object.id);
+                        keep = true;
+                    }
+                    return keep;
+                });
+
+                $scope.types.sort(function (A, B) {
+                    return (A.object.id > B.object.id) ? 1 : -1;
+                });
+                $scope.things.sort(function (A, B) {
+                    return (A.object.id > B.object.id) ? 1 : -1;
+                });
+
+                $scope.thingsFiltered = $scope.applyFilter();
+            };
+
+            $scope.update();
+        }
+    };
+}
 

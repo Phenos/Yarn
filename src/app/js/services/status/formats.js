4cac85d4c3561337ca872b9c1530880bcba2e1bc
mathieu <mathieu.sylvain@phenos.ca>
2016-03-20 17:24:54 -0400

    Minor fix to time of day

--- src/app/js/services/status/formats.js
+++ src/app/js/services/status/formats.js
@@ -2,7 +2,7 @@ yarn.service("statusFormats", function () {
 
     function timeOfDay(value) {
         var d = new Date(1976, 01, 01, 0, value, 0, 0);
-        return d.getHours() + ":" + d.getMinutes();
+        return d.getHours() + ":" + ("00" + d.getMinutes()).substring(0, 2);
     }
 
     return {

94c88375cda648a837aba865caf1f27d10f6e51f
mathieu <mathieu.sylvain@phenos.ca>
2016-03-22 15:25:39 -0400

    Removed unused files

--- src/app/js/services/tools/things.js
+++
@@ -1,8 +0,0 @@
-yarn.service("thingsTool", function thingsToolService(Tool) {
-    return new Tool("things", {
-        label: "Things",
-        icon: "thing",
-        directive: "things-tool",
-        order: 120
-    });
-});

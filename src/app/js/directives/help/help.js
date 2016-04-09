953652198676c70360aa22c035fb2a08921fd31c
mathieu <mathieu.sylvain@phenos.ca>
2016-04-09 13:57:30 -0400

    Added help for theming

--- src/app/js/directives/help/help.js
+++ src/app/js/directives/help/help.js
@@ -41,12 +41,9 @@
 
             this.load = function (uri) {
                 this.lastHelpFileLoaded(uri);
-
-                var self = this;
                 var resolvedURI = URI(uri).absoluteTo("./help/").toString();
 
-
-                //console.log("resolvedURI", resolvedURI);
+//                console.log("resolvedURI", resolvedURI);
 
                 var config = {
                     method: 'GET',
@@ -56,7 +53,7 @@
 
                 function then(response) {
                     self.setContent(response.data);
-                    //console.log("response", response);
+//                    console.log("response", response);
 
                     return {
                         source: response.data,
@@ -69,8 +66,10 @@
 
             $element.on("click", function (e) {
                 var target = e.target;
-                //console.log("-", e);
-                if (target.parentElement.tagName === "A") target = target.parentElement;
+//                console.log("-", e);
+                if (target.parentElement.tagName === "A") {
+                    target = target.parentElement;
+                }
                 if (target.tagName === "A") {
                     e.preventDefault();
                     var $target = angular.element(target);
@@ -114,7 +113,7 @@
         };
 
         service.focus = function () {
-            //console.log("focus!");
+//            console.log("focus!");
             controller.focus();
             return this;
         };

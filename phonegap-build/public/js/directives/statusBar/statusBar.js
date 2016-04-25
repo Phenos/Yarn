951489651d5835829f9db95d9d523eb1901b6cef
mathieu <mathieu.sylvain@phenos.ca>
2016-04-25 14:25:18 -0400

    Cordova setup - mobile fixes

--- phonegap-build/public/js/directives/statusBar/statusBar.js
+++ phonegap-build/public/js/directives/statusBar/statusBar.js
@@ -4,7 +4,7 @@ yarn.directive('statusBar', function StatusBarDirective(storyLog,
         restrict: 'E',
         scope: {},
         replace: true,
-        template:'<md-toolbar class=statusBar ng-if="allStatuses.length > 0 && visible"><div class=md-toolbar-tools><div flex=grow layout=row layout-align="center center"><md-button ng-repeat="status in allStatuses" ng-click=click(status)><md-icon ng-if=status.standardIcon md-svg-icon="/svg-icons/status/{{ status.standardIcon }}.svg"></md-icon><md-icon ng-if="status.SVGIcon && !status.standardIcon" md-svg-icon="{{ status.SVGIcon }}"></md-icon><span ng-if="!status.standardIcon && !status.SVGIcon">{{ status.name }} :&nbsp;</span> <span>{{ status.formattedValue() }}</span><md-tooltip md-direction=top>{{ status.name }}</md-tooltip></md-button></div></div></md-toolbar>',
+        template:'<md-toolbar class=statusBar ng-if="allStatuses.length > 0 && visible"><div class=md-toolbar-tools><div flex=grow layout=row layout-align="center center"><md-button ng-repeat="status in allStatuses" ng-click=click(status)><md-icon ng-if=status.standardIcon md-svg-icon="./svg-icons/status/{{ status.standardIcon }}.svg"></md-icon><md-icon ng-if="status.SVGIcon && !status.standardIcon" md-svg-icon="{{ status.SVGIcon }}"></md-icon><span ng-if="!status.standardIcon && !status.SVGIcon">{{ status.name }} :&nbsp;</span> <span>{{ status.formattedValue() }}</span><md-tooltip md-direction=top>{{ status.name }}</md-tooltip></md-button></div></div></md-toolbar>',
         controller: StatusBarController
     };
 
--- phonegap-build/public/js/directives/tools/assertions.js
+++ phonegap-build/public/js/directives/tools/assertions.js
@@ -6,7 +6,7 @@ yarn.directive('assertionsTool', function AssertionsTool() {
             isFocused: "="
         },
         replace: true,
-        template:'<div flex=100 layout=column><md-toolbar class=toolsToolbar flex=noshrink><div class=md-toolbar-tools><md-button ng-click=update() class=md-icon-button aria-label=Refresh><md-icon md-svg-icon=/svg-icons/refresh.svg></md-icon><md-tooltip>Refresh</md-tooltip></md-button></div></md-toolbar><div flex=grow layout=column><assertion-grid ng-if=isFocused flex=grow layout=column data=allAssertions></assertion-grid></div></div>',
+        template:'<div flex=100 layout=column><md-toolbar class=toolsToolbar flex=noshrink><div class=md-toolbar-tools><md-button ng-click=update() class=md-icon-button aria-label=Refresh><md-icon md-svg-icon=./svg-icons/refresh.svg></md-icon><md-tooltip>Refresh</md-tooltip></md-button></div></md-toolbar><div flex=grow layout=column><assertion-grid ng-if=isFocused flex=grow layout=column data=allAssertions></assertion-grid></div></div>',
         controller: function Controller($scope, state, channel) {
             $scope.allAssertions = [];
 
